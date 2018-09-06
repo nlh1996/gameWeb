53b13ff33c029a4609ab7d999e9933b7fb791324
One-AnDong <one-andong@foxmail.com>
2018-09-06 21:22:22 +0800

    头部导航1.0.1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,52 +1,157 @@
 <template>
-    <div class="head">
-        <el-row>
-            <el-col span="6">
-                <img :src="url">
-            </el-col>
+  <div class="head">
+    <el-row>
 
-            <el-col span="18">
-                <ul class="daohang">
-                    <li><router-link to="/">首页</router-link></li>
-                    <li><router-link to="/error">个人中心</router-link></li>
-                    <li><router-link to="/error">论坛</router-link></li>
-                    <li><router-link to="/error">充值</router-link></li>
-                </ul>
-            </el-col>
-        </el-row>
-    </div>
+      <!-- S 头部logo模块 -->
+      <el-col span="6"
+              class="left">
+
+        <h1 class="logo">
+          <img class=“logo__img”
+               :src="url">
+        </h1>
+
+      </el-col>
+      <!-- E 头部logo模块  -->
+
+      <!-- S 头部导航模块 -->
+      <el-col span="18"
+              class="right">
+        <ul class="nav clearfix"
+            @mouseout="setMouseOut">
+
+          <li class="nav__item"
+              v-for="item in navData"
+              :key="item.key"
+              @mouseover="setMouseOver(item)"
+              @click="setClick(item)">
+            <router-link class="nav__item-link"
+                         :class="{'nav__item_current':item.state}"
+                         :to="{name:item.router}">{{ item.name }}</router-link>
+          </li>
+
+        </ul>
+        <!-- /头部导航模块 -->
+
+        <span class="slide"
+              :style="slideObject"></span>
+        <!-- /滑动条 -->
+
+      </el-col>
+      <!-- E 头部导航模块 -->
+
+    </el-row>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                url: require('../assets/head/logo.png')
-            }
-        },
+export default {
+  data () {
+    return {
+      url: require('../assets/head/logo.png'),
+      navData: [
+        { key: 0, name: '首页', router: 'view', state: true },
+        { key: 1, name: '个人中心', router: 'view', state: false },
+        { key: 2, name: '论坛', router: 'error', state: false },
+        { key: 3, name: '充值', router: 'error', state: false },
+      ],
+      slideObject: {
+        left: '0'
+      }
     }
+  },
+  methods: {
+    /**
+     * @param item    object[必选]  当前按钮的对象数据
+     */
+    setMouseOver (item) {
+      this.slideObject['left'] = item.key * 100 + 'px'
+    },
+    setMouseOut () {
+      //使用filter筛选出当前处于活动状态的按钮，获得该对象
+      const current = this.navData.filter(o => o.state === true)
+      this.slideObject['left'] = current[0].key * 100 + 'px'
+    },
+    setClick (item) {
+      this.navData.forEach(o => { o.state = false })
+      const current = this.navData.filter(o => o.key === item.key)
+      current[0].state = true
+    }
+  }
+}
 </script>
 
-<style scoped>
-.daoahng ul,li{
-    padding: 10px;
-    list-style: none;
-    display: inline;
-    font-size: 18px;
+<style lang="less" scoped>
+/* CSS reset */
+.clearfix:after {
+  display: block;
+  content: '';
+  height: 0;
+  visibility: hidden;
+  clear: both;
 }
-/* 指针移动到链接上 */
-a:hover {         
-  color: white;  
-  height: 30px;  
-  background: #f00;
+ul,
+h1 {
+  padding: 0;
+  margin: 0;
 }
-/* 被点击 */
-a:active{  
-    color: #000;  
+@bg: #f68121;
+/* 主体布局 */
+.head {
+  width: 1200px;
+  margin: 0 auto;
 }
-/* 已被访问 */
-a:visited {
-     color: #a369af;
+.right {
+  position: relative;
 }
 
+/* 头部logo导航模块
+-------------------------------------------- */
+.logo {
+  padding-top: 8px;
+  width: 124px;
+  height: 54px;
+}
+.logo__img {
+  display: block;
+}
+
+/* 头部导航模块
+-------------------------------------------- */
+.nav {
+  list-style: none;
+}
+.nav__item {
+  float: left;
+  width: 100px;
+  height: 58px;
+  padding-top: 4px;
+  line-height: 58px;
+  text-align: center;
+  &:hover {
+    .nav__item-link {
+      color: @bg;
+    }
+  }
+}
+.nav__item-link {
+  font-family: 'Microsoft YaHei', sans-serif;
+  font-size: 18px;
+  text-decoration: none;
+  color: #333;
+  cursor: pointer;
+}
+.nav__item_current {
+  color: @bg;
+}
+/* 滑块 */
+.slide {
+  position: absolute;
+  transition: all 0.3s;
+  bottom: 0;
+  // left: 0;
+  height: 2px;
+  width: 100px;
+  background: @bg;
+}
 </style>
--- src/components/View.vue
+++ src/components/View.vue
@@ -1,50 +1,52 @@
 <template>
-    <div class="view">
-        <el-row>         
-            <headlist></headlist>
-        </el-row>
+  <div class="view">
+    <el-row class="header">
+      <headlist></headlist>
+    </el-row>
 
-        <el-row>
-            <div>
-                <el-col span="24">
-                    <carousel></carousel>
-                </el-col>
-            </div>
-     
-        </el-row>
+    <el-row>
+      <div>
+        <el-col span="24">
+          <carousel></carousel>
+        </el-col>
+      </div>
 
-        <el-row>
-            <el-col span="18">
-                <div>
-                    <gamelist></gamelist>
-                </div>
-            </el-col>
+    </el-row>
 
-            <el-col span="6">
-                <div class="login">
-                    
-                </div>
-            </el-col>
-         </el-row>
-    </div>
+    <el-row>
+      <el-col span="18">
+        <div>
+          <gamelist></gamelist>
+        </div>
+      </el-col>
+
+      <el-col span="6">
+        <div class="login">
+
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>  
-    import gamelist from '@/components/GameList'
-    import headlist from '@/components/Header'
-    import carousel from '@/components/Carousel'
+import gamelist from '@/components/GameList'
+import headlist from '@/components/Header'
+import carousel from '@/components/Carousel'
 
-    export default {
-        name: 'main',
-        components:{
-            gamelist,
-            headlist,
-            carousel
-        }    
-    }
+export default {
+  name: 'main',
+  components: {
+    gamelist,
+    headlist,
+    carousel
+  }
+}
 </script>
 
 <style scoped>
-
-
+.header {
+  border-top: 1px solid #d2d5da;
+  background: #fff url('../assets/head/platform_header_bg.jpg') repeat-x;
+}
 </style>
